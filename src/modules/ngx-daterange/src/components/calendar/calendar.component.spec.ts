2fab9d19eba4577ea30c48a9548e39120b54dd4d
Brandon Taylor <alsoicode@gmail.com>
2018-11-18 11:39:32 -0500

    Added test for getWeekNumbers()

--- src/modules/ngx-daterange/src/components/calendar/calendar.component.spec.ts
+++ src/modules/ngx-daterange/src/components/calendar/calendar.component.spec.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-import * as moment from 'moment';
+import * as momentNs from 'moment'; const moment = momentNs;
+
+import { extendMoment, DateRange } from 'moment-range';
+
+const { range } = extendMoment(moment);
 
 import { CalendarComponent } from '../calendar/calendar.component';
 import { FormatMomentDatePipe } from '../../pipes/format-moment-date.pipe';
@@ -41,4 +45,26 @@ describe('Testing CalendarComponent', () => {
 
     expect(component).toBeDefined();
   }));
+
+  describe('Test range generation utility functions', () => {
+    it('calling getWeekNumbers() should return an array of the weeks of the year for the given range of months', async(() => {
+      let firstDay = moment([2018, 0]).startOf('month');
+      let endDay = moment([2018, 1]).add(1, 'month').endOf('month');
+      let monthRange = range(firstDay, endDay);
+
+      let weekNumbers = component.getWeekNumbers(monthRange);
+      let expectedWeeks: number[] = [1, 2, 3, 4, 5];
+
+      expect(weekNumbers).toEqual(expectedWeeks);
+
+
+      firstDay = moment([2018, 1]).startOf('month');
+      endDay = moment([2018, 2]).add(1, 'month').endOf('month');
+      monthRange = range(firstDay, endDay);
+      weekNumbers = component.getWeekNumbers(monthRange);
+      expectedWeeks = [5, 6, 7, 8, 9];
+
+      expect(weekNumbers).toEqual(expectedWeeks);
+    }));
+  });
 });
