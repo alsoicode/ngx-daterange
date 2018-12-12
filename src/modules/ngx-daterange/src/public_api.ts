5be6ee9a872f56e53b5e90bf2363093f11c25620
Brandon Taylor <alsoicode@gmail.com>
2018-12-12 11:48:55 -0500

    Exposed components in public api. Added example of calling reset on a date picker instance to clear the value.

--- src/modules/ngx-daterange/src/public_api.ts
+++ src/modules/ngx-daterange/src/public_api.ts
@@ -1,2 +1,4 @@
 export { NgxDateRangeModule } from './ngx-daterange.module';
+export { CalendarComponent } from './components/calendar/calendar.component';
+export { DateRangePickerComponent } from './components/datepicker/date-range-picker.component';
 export { IDateRangePickerOptions, IDefinedDateRange } from './interfaces';
