57d03068b6c1fdec2c19ac616d2fb38fb14929ec
Brandon Taylor <alsoicode@gmail.com>
2020-07-13 11:16:11 -0400

    - Added mobile device detection - Added styling for mobile - Hid manual inputs for mobile

--- src/modules/ngx-daterange/src/components/datepicker/date-range-picker.component.ts
+++ src/modules/ngx-daterange/src/components/datepicker/date-range-picker.component.ts
@@ -106,13 +106,12 @@ export class DateRangePickerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (navigator.userAgent.match(/Android/i)
-      || navigator.userAgent.match(/webOS/i)
-      || navigator.userAgent.match(/iPhone/i)
-      || navigator.userAgent.match(/iPad/i)
-      || navigator.userAgent.match(/iPod/i)
-      || navigator.userAgent.match(/BlackBerry/i)
-      || navigator.userAgent.match(/Windows Phone/i)) {
+    if (this.window.navigator.userAgent.match(/Android/i)
+      || this.window.navigator.userAgent.match(/webOS/i)
+      || this.window.navigator.userAgent.match(/iPhone/i)
+      || this.window.navigator.userAgent.match(/iPod/i)
+      || this.window.navigator.userAgent.match(/BlackBerry/i)
+      || this.window.navigator.userAgent.match(/Windows Phone/i)) {
       this.isMobile = true;
     }
 
