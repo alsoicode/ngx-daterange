9e5ebc0ffb15a9b9ba806144bb409150985d133c
Brandon Taylor <alsoicode@users.noreply.github.com>
2020-12-05 15:24:12 -0500

    bugfix / Ensure values are Moment instances (#54)
    - Version bump to 1.0.37
    - Added typeof checks in format-moment-date pipe and ngOnChanges in calendar component to maintain data consistency
    - Updated dateChanged handler to allow selecting a "from" date in the left calendar to emitRangeSelected if a "to" date is already selected

--- src/modules/ngx-daterange/src/pipes/format-moment-date.pipe.ts
+++ src/modules/ngx-daterange/src/pipes/format-moment-date.pipe.ts
@@ -1,11 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as momentNs from 'moment';
+const moment = momentNs;
 
 @Pipe({
   name: 'formatMomentDate'
 })
 export class FormatMomentDatePipe implements PipeTransform {
   transform(value: momentNs.Moment, format: string): string {
-    return value ? value.format(format) : '';
+    let valueToFormat = value;
+
+    if (valueToFormat && !momentNs.isMoment(valueToFormat)) {
+      valueToFormat = moment(valueToFormat);
+    }
+
+    return valueToFormat ? valueToFormat.format(format) : '';
   }
 }
